d43f9fed4125d9109beb1ddaf8a371d3cda5d6bb
Phoenix333 <85232209+mitsuhina43@users.noreply.github.com>
2022-07-23 13:23:17 +0700

    23072022

--- template.cpp
+++ template.cpp
@@ -1,36 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
 #define PB push_back
 #define ALL(i_) i_.begin(), i_.end()
-#define LOG2(x) (31 - __builtin_clz(x));
+#define LOG2(x) (31 - __builtin_clz(x))
+#define getBit(x, i) ((x >> i) & 1)
+#define rd(l, r) (l + rng() % (r - l + 1))
 
 typedef long long ll;
 typedef long double ld;
 typedef pair<int, int> ii;
 
-template<class X, class Y>
-    bool minimize(X &x, const Y &y) {
-        X eps = 1e-9;
-        if (x > y + eps) {
-            x = y;
-            return true;
-        } else return false;
+template<class X, class Y> bool minimize(X &x, const Y &y) {
+    X eps = 1e-9;
+    if (x > y + eps){
+        x = y;
+        return 1;
     }
+    return 0;
+}
 
-template<class X, class Y>
-    bool maximize(X &x, const Y &y) {
-        X eps = 1e-9;
-        if (x + eps < y) {
-            x = y;
-            return true;
-        } else return false;
+template<class X, class Y> bool maximize(X &x, const Y &y) {
+    X eps = 1e-9;
+    if (x + eps < y){
+        x = y;
+        return 1;
     }
+    return 0;
+}
 
-template<class T>
-    T Abs(const T &x) {
-        return (x < 0 ? -x : x);
-    }
+template<class T> T Abs(const T &x) { return (x < 0 ? -x : x); }
 
 const int mod = (int) 1e9 + 7;
 const int oo = (int) 1e9 + 99;
@@ -46,6 +46,5 @@ int main(){
         freopen(TASK".inp", "r", stdin);
         freopen(TASK".out", "w", stdout);
     }
-
     return 0;
 }
